2c9de00d434e24b6c2fddd446b0e6fed6923424a
taiseen <taiseen.cse@gmail.com>
2022-02-05 22:25:27 +0600

    work ==> OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 // 5 - Feb - 2022 
 SVGInject(document.querySelectorAll("img.injectable"));
 
-// const body = document.body;
-const menuBar = document.querySelector('.menuBar');
-const themeBtn = document.querySelector('.themeToggle');
-const navbar = document.querySelector('.navbar');
+// let body = document.body;
+let menuBar = document.querySelector('.menuBar');
+let themeBtn = document.querySelector('.themeToggle');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 
 // themeBtn.addEventListener('click', () => {
@@ -22,6 +25,24 @@ menuBar.addEventListener('click', () => {
 window.addEventListener('scroll', () => {
     menuBar.classList.remove('fa-times');
     navbar.classList.remove('userClick');
+
+
+    // auto select menu according to screen scrolling
+    sections.forEach(section => {
+
+        let top = window.scrollY;
+        let height = section.offsetHeight;
+        let offset = section.offsetTop - 150;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
 });
 
 
@@ -47,7 +68,7 @@ switchBtn.addEventListener('click', () => {
         // clicking upon selected color
         btn.addEventListener('click', () => {
             root.style.setProperty('--mainColor', btn.style.backgroundColor);
-            
+
             // after select a color, self close color palate... 
             colorSwitcher.classList.remove('userClick');
         })
